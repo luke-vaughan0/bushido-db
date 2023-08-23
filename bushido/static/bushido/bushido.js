08779eecfd4b5e0c8a6034d0443f0d5a0ed9f32d
Ellie <lv.forgewood@gmail.com>
2023-08-23 16:45:28 +0100

    search improvements

--- bushido/static/bushido/bushido.js
+++ bushido/static/bushido/bushido.js
@@ -1,5 +1,5 @@
 $(document).ready( function () {
-    var panes = $('.search-panes')
+    var panes = $('.search-panes');
     var table = $('.data-table').DataTable({
         "pageLength": 100,
         "lengthMenu": [ [10, 25, 50, 100, -1], [10, 25, 50, 100, "All"] ],
@@ -20,6 +20,7 @@ $(document).ready( function () {
 $('body').on('keypress', function(keyPress) {
     var input = $('input[name="search"]');
     if(!$(':focus').length && !input.is(':focus') && keyPress.key.match(/^[0-9a-zA-Z]+$/)) {
+        input.val("");
         input.trigger("focus");
     }
 });
