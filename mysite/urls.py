1e82fb504f418a7b85cbbd4e1f8344ceb859fec9
Luke <luke@swiftrefurbishments.co.uk>
2023-08-24 16:38:30 +0100

    redirect from root

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(pattern_name='bushido:index')),
     path("bushido/", include("bushido.urls")),
     path('admin/', admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
